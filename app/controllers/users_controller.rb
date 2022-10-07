86dc6ddde70d3e1a2e0ad767fabe7ae407c1ae19
GabrielVieiraDw <gabrielvieiradw@gmail.com>
2022-10-07 17:03:15 -0300

    Add authenticatio method, current_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to G Blog #{@user.user_name} !"
       redirect_to articles_path
     else
